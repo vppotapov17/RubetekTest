9a849535592b460471b45e798e0dbe539818b318
vppotapov17 <vppotapov17@yandex.ru>
2022-06-09 11:56:20 +0300

    Test

---
+++ app/src/main/java/com/example/rubetektest/DomophoneActivity.java
@@ -0,0 +1,20 @@
+package com.example.rubetektest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+public class DomophoneActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+}
